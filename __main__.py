42961271129485e03621e7327cc1a27e2bb9f9d8
Maris@physik.hu-berlin <maris.baier@physik.hu-berlin.de>
2023-05-07 22:01:30 +0200

    created new SBAHNS method

--- __main__.py
+++ __main__.py
@@ -9,6 +9,10 @@ class SbahnObject:
         self.direction = canvas.create_text(230, ypos, text=direction,font=('Helvetica',30,'bold'), anchor='w')
         self.when = canvas.create_text(640, ypos, text=when,font=('Helvetica',30,'bold'), anchor='w')
         self.hintimage = canvas.create_image(780,ypos, image=hintimage)
+    def change(self, image, direction, when):
+        canvas.itemconfig(self.image, image = image)
+        canvas.itemconfig(self.direction, text=direction)
+        canvas.itemconfig(self.when, text=when)
 
 def getDepartures():
     while True:
@@ -27,14 +31,8 @@ def getDepartures():
             break
     return Abfahrten
 
-def displaySBahns(SBahns):
-    add = len(displayedobjects)
-    for i,Sbahn in enumerate(SBahns):
-        i += add
-        linename = Sbahn['line']['name']
-        lineimage = images[linename]
-        when = int(Sbahn['when'][14]+Sbahn['when'][15])-int(datetime.now().strftime("%M"))-60*(int(datetime.now().hour)-int(Sbahn['when'][12])-10*int(Sbahn['when'][11]))
-        displayedobjects.append(SbahnObject(lineimage,Sbahn['direction'],when,hintimage=Empty,ypos=100+i*60))
+def wheninminutes(SbahnJson):
+    return int(SbahnJson['when'][14]+SbahnJson['when'][15])-int(datetime.now().strftime("%M"))-60*(int(datetime.now().hour)-int(SbahnJson['when'][12])-10*int(SbahnJson['when'][11]))
 
 ### Setup
 
@@ -48,7 +46,6 @@ images = {imagename:ImageTk.PhotoImage(Image.open('src/images/'+imagename+'.png'
 
 displayedobjects = []
 
-#displaySBahns(getDepartures())
 n=0
 canvas.create_text(640, 55, text='min', font=('Helvetica',25,'bold'))
 canvas.pack(fill=tk.BOTH, expand=True)
@@ -56,31 +53,30 @@ canvas.pack(fill=tk.BOTH, expand=True)
 def mainloop():
     Sbahnabfahrten = getDepartures()
     if len(Sbahnabfahrten)>len(displayedobjects):
-        #displaySBahns(Sbahnabfahrten[len(displayedobjects):-1])
         add = len(displayedobjects)
         print(len(displayedobjects))
         for i,Sbahn in enumerate(Sbahnabfahrten[len(displayedobjects):-1]):
             i += add
             linename = Sbahn['line']['name']
             lineimage = images[linename]
-            when = int(Sbahn['when'][14]+Sbahn['when'][15])-int(datetime.now().strftime("%M"))-60*(int(datetime.now().hour)-int(Sbahn['when'][12])-10*int(Sbahn['when'][11]))
-            displayedobjects.append(SbahnObject(lineimage,Sbahn['direction'],when,hintimage=Empty,ypos=100+i*60))
+            wheninminutes = wheninminutes(Sbahn)
+            displayedobjects.append(SbahnObject(lineimage,Sbahn['direction'],wheninminutes,hintimage=Empty,ypos=100+i*60))
+
     for i,displayedobject in enumerate(displayedobjects):
+
         if i<len(Sbahnabfahrten):
+
             Sbahn = Sbahnabfahrten[i]
             linename = Sbahn['line']['name']
-            inminutes=int(Sbahn['when'][14]+Sbahn['when'][15])-int(datetime.now().strftime("%M"))-60*(int(datetime.now().hour)-int(Sbahn['when'][12])-10*int(Sbahn['when'][11]))
-            canvas.itemconfig(displayedobject.image, image = images[linename])
-            canvas.itemconfig(displayedobject.direction, text = Sbahn['direction'])
-            canvas.itemconfig(displayedobject.when, text=inminutes)
+            inminutes = wheninminutes(Sbahn)
+            displayedobject.change(images[linename],Sbahn['direction'],inminutes)
             if inminutes<11:
                 canvas.itemconfig(displayedobject.when, fill='red')
             else:
                 canvas.itemconfig(displayedobject.when, fill='black')
+
         else:
-            canvas.itemconfig(displayedobject.image, image = Empty)
-            canvas.itemconfig(displayedobject.direction, text = '')
-            canvas.itemconfig(displayedobject.when, text='')
+            displayedobject.change(Empty,'','')
     global n
     n+=1
     print('updated ',n, 'times')
