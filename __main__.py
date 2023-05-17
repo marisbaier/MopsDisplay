823b71da5784d805252817a22f5cb962e62009c7
Ravy <hazel@farfrom.earth>
2023-05-17 20:57:41 +0200

    lint: more code style, use reduce() on lists for reductions

--- __main__.py
+++ __main__.py
@@ -8,8 +8,10 @@ import os
 import re
 import pathlib
 import tkinter as tk
+from functools import reduce
 from datetime import datetime
 import requests
+
 
 from PIL import ImageTk, Image
 
@@ -23,7 +25,7 @@ class SbahnObject:
         try:
             lineimage = images[requestjson['line']['name']]
         except KeyError:
-            lineimage = Empty
+            lineimage = empty
         self.image = canvas.create_image(130, ypos, image = lineimage)
 
         direct = requestjson['direction']
@@ -31,7 +33,7 @@ class SbahnObject:
             direct = direct[0:15] + "..."
             print(direct)
 
-        self.when_int = wheninminutes(requestjson)
+        self.when_int = when_in_minutes(requestjson)
 
         self.direction = canvas.create_text(170, ypos, text=direct,font=('Helvetica',20,'bold'), anchor='w')
         self.when = canvas.create_text(480, ypos, text=self.when_int,font=('Helvetica',20,'bold'), anchor='w')
@@ -72,7 +74,7 @@ class Station:
             if i<len(s_bahn_departures):
                 s_bahn = s_bahn_departures[i]
                 linename = s_bahn['line']['name']
-                inminutes = wheninminutes(s_bahn)
+                inminutes = when_in_minutes(s_bahn)
                 
                 direct = s_bahn['direction']
                 
@@ -86,13 +88,13 @@ class Station:
                     if re.match("[0-9]{3}", linename) or s_bahn['line']['adminCode'] == "SEV":
                         displayedobject.change(images['164'],direct,inminutes)
                     else:
-                        displayedobject.change(Empty,direct,inminutes)
+                        displayedobject.change(empty,direct,inminutes)
                 if inminutes<self.distance:
                     canvas.itemconfig(displayedobject.when, fill='red')
                 else:
                     canvas.itemconfig(displayedobject.when, fill='black')
             else:
-                displayedobject.change(Empty,'','')
+                displayedobject.change(empty,'','')
     
     def get_dep_list_length(self):
         return len(self.dObs)
@@ -116,34 +118,40 @@ def get_departures(url, max_departures):
             break
     return departures
 
-def wheninminutes(SbahnJson):
+def when_in_minutes(SbahnJson):
     if int(datetime.now().hour) == 23:
         return int(SbahnJson['when'][14]+SbahnJson['when'][15])-int(datetime.now().strftime("%M"))+60
 
     return int(SbahnJson['when'][14]+SbahnJson['when'][15])-int(datetime.now().strftime("%M"))-60*(int(datetime.now().hour)-int(SbahnJson['when'][12])-10*int(SbahnJson['when'][11]))
 
-def getImages():
+def get_images():
     out = []
 
     # https://stackoverflow.com/a/3430395
     root_path = pathlib.Path(__file__).parent.resolve()
 
     for f in os.scandir(root_path.joinpath("src/images/")):
-        if re.match("S?[0-9]+\.png", f.name):
+        if re.match(r"S?[0-9]+\.png", f.name):
             out.append(f.name.split(".")[0])
 
     return out
 
-def setup(canvas, images, HUlogoimage):
-    canvas.create_rectangle(580, 0, 1200, 800, fill='light blue', outline='light blue')
-    canvas.create_image(700, 100, image=HUlogoimage)
-    #canvas.create_text(480, 55, text='min', font=('Helvetica',15,'bold'))           # min sign
-    canvas.pack(fill=tk.BOTH, expand=True)
-    canvas.create_text(600, 300, text='Nächste Veranstaltungen:', font=('Helvetica', 18,'bold'), anchor='nw')
-    canvas.create_text(600, 350, text='Morgen Auftaktsparty ab 17 Uhr!', font=('Helvetica', 12,'bold'), anchor='nw')
-    canvas.create_text(600, 375, text='22. Mai   Schachturnier', font=('Helvetica', 12,'bold'), anchor='nw')
-    canvas.create_text(600, 400, text='30. Mai   Mops Geburtstag', font=('Helvetica', 12,'bold'), anchor='nw')
-    canvas.create_text(600, 425, text='''14. Juni  Hörsaalkino Special:\n         "Jim Knopf und Lukas\n           der Lokomotivführer"\n       mit Vortrag von Dr. Lohse''', font=('Helvetica', 12,'bold'), anchor='nw')
+def setup(ctx):
+    ctx.create_rectangle(580, 0, 1200, 800, fill='light blue', outline='light blue')
+    ctx.create_image(700, 100, image=hu_logo_image)
+    #ctx.create_text(480, 55, text='min', font=('Helvetica',15,'bold'))           # min sign
+    ctx.pack(fill=tk.BOTH, expand=True)
+    ctx.create_text(600, 300, text='Nächste Veranstaltungen:', font=('Helvetica', 18,'bold'), anchor='nw')
+    ctx.create_text(600, 350, text='Morgen Auftaktsparty ab 17 Uhr!', font=('Helvetica', 12,'bold'), anchor='nw')
+    ctx.create_text(600, 375, text='22. Mai   Schachturnier', font=('Helvetica', 12,'bold'), anchor='nw')
+    ctx.create_text(600, 400, text='30. Mai   Mops Geburtstag', font=('Helvetica', 12,'bold'), anchor='nw')
+    ctx.create_text(600, 425, text='''14. Juni  Hörsaalkino Special:\n         "Jim Knopf und Lukas\n           der Lokomotivführer"\n       mit Vortrag von Dr. Lohse''', font=('Helvetica', 12,'bold'), anchor='nw')
+
+def load_image(acc, name):
+    image = Image.open(image_path.joinpath(f"{name}.png")).resize((40,20))
+    image = ImageTk.PhotoImage(image)
+    
+    return {**acc, **{name: image}}
 
 
 ### Setup
@@ -155,29 +163,33 @@ canvas = tk.Canvas()
 # https://stackoverflow.com/a/3430395
 image_path = pathlib.Path(__file__).parent.resolve().joinpath("src/images/")
 
-Empty = ImageTk.PhotoImage(Image.open(image_path.joinpath("Empty.png")).resize((1,1)))
-HUlogoimage = ImageTk.PhotoImage(Image.open(image_path.joinpath("Huberlin-logo.png")).resize((100,100)))
-imagenames = getImages()
-images = {imagename:ImageTk.PhotoImage(Image.open(image_path.joinpath(f"{imagename}.png")).resize((40,20))) for imagename in imagenames}
+empty = Image.open(image_path.joinpath("Empty.png")).resize((1,1))
+empty = ImageTk.PhotoImage(empty)
+
+hu_logo_image = Image.open(image_path.joinpath("Huberlin-logo.png")).resize((100,100))
+hu_logo_image = ImageTk.PhotoImage(hu_logo_image)
+
+imagenames = get_images()
+images = reduce(load_image, imagenames, {})
 
 
-setup(canvas, images, HUlogoimage)
+setup(canvas)
 
 s = [
-    ("S Adlershof",900193002, True, False, True,9,33, 11, 6),
-    ("Karl-Ziegler-Str",900000194016, False, True, False,3, 25, 5, 5),
-    ("Magnusstr.",900000194501,False, False, True,3, 21, 5, 5)
+    ("S Adlershof",900193002, True, False, True, 9, 33, 11, 6),
+    ("Karl-Ziegler-Str",900000194016, False, True, False, 3, 25, 5, 5),
+    ("Magnusstr.",900000194501, False, False, True, 3, 21, 5, 5)
 ]
 stations = []
-offset = 0
+station_display_offset = 0
 for j, sa in enumerate(s):
     if j > 0:
-        offset += stations[j-1].get_dep_list_length() + 1
-    stations.append(Station(sa[0],sa[1],sa[2],sa[3], sa[4], sa[5], sa[6], sa[7], sa[8], offset))
+        station_display_offset += stations[j-1].get_dep_list_length() + 1
+    stations.append(Station(sa[0],sa[1],sa[2],sa[3], sa[4], sa[5], sa[6], sa[7], sa[8], station_display_offset))
 
 def mainloop():
-    for s in stations[:1]:
-        s.departure_list()
+    for station in stations[:1]:
+        station.departure_list()
     root.after(60000, mainloop) #Wait for a minute
 
 root.after(1000, mainloop) # run first time after 1000ms (1s)
