eb8f6fc29f49e6c1a4edd150b1576685a416b2b5
Ravy <hazel@farfrom.earth>
2023-05-17 20:37:07 +0200

    lint: code style, unused/duplicate vars

--- __main__.py
+++ __main__.py
@@ -6,22 +6,23 @@ Author: Maris Baier
 
 import os
 import re
-import requests
 import pathlib
 import tkinter as tk
 from datetime import datetime
+import requests
 
 from PIL import ImageTk, Image
 
 class SbahnObject:
     def __init__(self, requestjson, ypos):
         '''
-        When an S-Bahn object is created, it checks for a correspanding lineimage stored. If there's none, it shows an empty image.
+        When an S-Bahn object is created, it checks for a correspanding lineimage stored.
+        If there's none, it shows an empty image.
         It then creates text (direction and departure in min).
         '''
         try:
             lineimage = images[requestjson['line']['name']]
-        except:
+        except KeyError:
             lineimage = Empty
         self.image = canvas.create_image(130, ypos, image = lineimage)
 
@@ -30,50 +31,50 @@ class SbahnObject:
             direct = direct[0:15] + "..."
             print(direct)
 
-        self.whenInt = wheninminutes(requestjson)
+        self.when_int = wheninminutes(requestjson)
 
         self.direction = canvas.create_text(170, ypos, text=direct,font=('Helvetica',20,'bold'), anchor='w')
-        self.when = canvas.create_text(480, ypos, text=self.whenInt,font=('Helvetica',20,'bold'), anchor='w')
+        self.when = canvas.create_text(480, ypos, text=self.when_int,font=('Helvetica',20,'bold'), anchor='w')
 
     def change(self, image, direction, when):
         canvas.itemconfig(self.image, image = image)
         canvas.itemconfig(self.direction, text=direction)
         canvas.itemconfig(self.when, text=when)
 
-class station:
-    def __init__(self, name, id, sBahn, Tram, Bus, startFrom=5, upTo=33, distance=11, maxDepatures=6, offset=0):
+class Station:
+    def __init__(self, name, station_id, s_bahn, tram, bus, start_from=5, up_to=33, distance=11, max_departures=6, display_offset=0):
         self.name = name
-        self.id = id
-        self.sBahn = sBahn
-        self.Tram = Tram
-        self.Bus = Bus
-        self.offset = offset
-        self.startFrom = startFrom
-        self.upTo = upTo
+        self.station_id = station_id
+        self.s_bahn = s_bahn
+        self.tram = tram
+        self.bus = bus
+        self.display_offset = display_offset
+        self.start_from = start_from
+        self.up_to = up_to
         self.distance = distance
-        self.maxDepatures = maxDepatures
+        self.max_departures = max_departures
 
         self.dObs = []
 
-        self.dObs.append(canvas.create_text(50, 100+self.offset*40, text=self.name,font=('Helvetica',28,'bold'), anchor='w'))
-        self.depatureList()
+        self.dObs.append(canvas.create_text(50, 100+self.display_offset*40, text=self.name,font=('Helvetica',28,'bold'), anchor='w'))
+        self.departure_list()
 
 
-    def depatureList(self):        
-        Sbahnabfahrten = getDepartures(self.getUrl(), self.maxDepatures)
-        if len(Sbahnabfahrten)>len(self.dObs):
+    def departure_list(self):
+        s_bahn_departures = get_departures(self.get_url(), self.max_departures)
+        if len(s_bahn_departures)>len(self.dObs):
             add = len(self.dObs)
-            for i,Sbahn in enumerate(Sbahnabfahrten[len(self.dObs):-1]):
+            for i,s_bahn in enumerate(s_bahn_departures[len(self.dObs):-1]):
                 i += add
-                self.dObs.append(SbahnObject(Sbahn, ypos=100+(i+self.offset)*40))
+                self.dObs.append(SbahnObject(s_bahn, ypos=100+(i+self.display_offset)*40))
 
         for i,displayedobject in enumerate(self.dObs[1:]):
-            if i<len(Sbahnabfahrten):
-                Sbahn = Sbahnabfahrten[i]
-                linename = Sbahn['line']['name']
-                inminutes = wheninminutes(Sbahn)
+            if i<len(s_bahn_departures):
+                s_bahn = s_bahn_departures[i]
+                linename = s_bahn['line']['name']
+                inminutes = wheninminutes(s_bahn)
                 
-                direct = Sbahn['direction']
+                direct = s_bahn['direction']
                 
                 if len(direct) > 15:
                     direct = direct[:15] + "..."
@@ -82,7 +83,7 @@ class station:
                 try:
                     displayedobject.change(images[linename],direct,inminutes)
                 except KeyError:
-                    if re.match("[0-9]{3}", linename) or Sbahn['line']['adminCode'] == "SEV":
+                    if re.match("[0-9]{3}", linename) or s_bahn['line']['adminCode'] == "SEV":
                         displayedobject.change(images['164'],direct,inminutes)
                     else:
                         displayedobject.change(Empty,direct,inminutes)
@@ -93,27 +94,27 @@ class station:
             else:
                 displayedobject.change(Empty,'','')
     
-    def getDepListLength(self):
+    def get_dep_list_length(self):
         return len(self.dObs)
 
-    def getUrl(self):
-        return f"https://v5.bvg.transport.rest/stops/{self.id}/departures?results=20&suburban={self.sBahn}&tram={self.Tram}&bus={self.Bus}&when=in+{self.startFrom}+minutes&duration={self.upTo}"
+    def get_url(self):
+        return f"https://v5.bvg.transport.rest/stops/{self.station_id}/departures?results=20&suburban={self.s_bahn}&tram={self.tram}&bus={self.bus}&when=in+{self.start_from}+minutes&duration={self.up_to}"
 
-def getDepartures(url, maxDepatures):
+def get_departures(url, max_departures):
     while True:
         try:
-            response = requests.get(url).json()
-        except:
+            response = requests.get(url, timeout=30000).json()
+        except requests.exceptions.Timeout:
             continue
         else:
-            Abfahrten=[]
-            tripIds=[]
+            departures = []
+            trip_ids = []
             for i in response:
-                if i['when'] is not None and i['tripId'] not in tripIds and len(Abfahrten)<=maxDepatures:
-                    Abfahrten.append(i)
-                    tripIds.append(i['tripId'])
+                if i['when'] is not None and i['tripId'] not in trip_ids and len(departures) <= max_departures:
+                    departures.append(i)
+                    trip_ids.append(i['tripId'])
             break
-    return Abfahrten
+    return departures
 
 def wheninminutes(SbahnJson):
     if int(datetime.now().hour) == 23:
@@ -134,8 +135,8 @@ def getImages():
     return out
 
 def setup(canvas, images, HUlogoimage):
-    bluecolorbox = canvas.create_rectangle(580, 0, 1200, 800, fill='light blue', outline='light blue')
-    HUlogo = canvas.create_image(700, 100, image=HUlogoimage)
+    canvas.create_rectangle(580, 0, 1200, 800, fill='light blue', outline='light blue')
+    canvas.create_image(700, 100, image=HUlogoimage)
     #canvas.create_text(480, 55, text='min', font=('Helvetica',15,'bold'))           # min sign
     canvas.pack(fill=tk.BOTH, expand=True)
     canvas.create_text(600, 300, text='Nächste Veranstaltungen:', font=('Helvetica', 18,'bold'), anchor='nw')
@@ -162,7 +163,6 @@ images = {imagename:ImageTk.PhotoImage(Image.open(image_path.joinpath(f"{imagena
 
 setup(canvas, images, HUlogoimage)
 
-global stations
 s = [
     ("S Adlershof",900193002, True, False, True,9,33, 11, 6),
     ("Karl-Ziegler-Str",900000194016, False, True, False,3, 25, 5, 5),
@@ -172,12 +172,12 @@ stations = []
 offset = 0
 for j, sa in enumerate(s):
     if j > 0:
-        offset += stations[j-1].getDepListLength() + 1
-    stations.append(station(sa[0],sa[1],sa[2],sa[3], sa[4], sa[5], sa[6], sa[7], sa[8], offset))
+        offset += stations[j-1].get_dep_list_length() + 1
+    stations.append(Station(sa[0],sa[1],sa[2],sa[3], sa[4], sa[5], sa[6], sa[7], sa[8], offset))
 
 def mainloop():
     for s in stations[:1]:
-        s.depatureList()
+        s.departure_list()
     root.after(60000, mainloop) #Wait for a minute
 
 root.after(1000, mainloop) # run first time after 1000ms (1s)
